a52376ff99422d97465bf609c1d05f9d40dfd9f0
ximliu <ximliu0228aicmz@gmail.com>
2019-03-22 17:44:52 +0800

    Update install-release.sh

--- install-release.sh
+++ install-release.sh
@@ -128,7 +128,7 @@ downloadV2Ray(){
     rm -rf /tmp/v2ray
     mkdir -p /tmp/v2ray
     colorEcho ${BLUE} "Downloading V2Ray."
-    DOWNLOAD_LINK="https://github.com/alliswell2day/v2/raw/release/v2ray-linux-${VDIS}.zip"
+    DOWNLOAD_LINK="https://github.com/ximliu/v2/raw/release/v2ray-linux-${VDIS}.zip"
     curl ${PROXY} -L -H "Cache-Control: no-cache" -o ${ZIPFILE} ${DOWNLOAD_LINK}
     if [ $? != 0 ];then
         colorEcho ${RED} "Failed to download! Please check your network or try again."
@@ -210,7 +210,7 @@ getVersion(){
         if [[ ${CUR_VER} != v* ]]; then
             CUR_VER=v${CUR_VER}
         fi
-        TAG_URL="https://api.github.com/repos/alliswell2day/v2ray-core/raw/releases"
+        TAG_URL="https://api.github.com/repos/ximliu/alliswell2day-v2ray-core-back-up/raw/releases"
         NEW_VER=`curl ${PROXY} -s ${TAG_URL} --connect-timeout 10| grep 'tag_name' | cut -d\" -f4`
         if [[ ${NEW_VER} != v* ]]; then
           NEW_VER=v${NEW_VER}
